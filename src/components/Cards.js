262cb06b25bfdcf056c8ba9320c3ca5e44219987
kejin geng <kejingeng@kejins-MacBook-Pro.local>
2021-11-19 00:57:03 -0800

    Update UI Code

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import CourseGoalList from './ShowList/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './ShowList/CourseGoals/CourseInput/CourseInput';
@@ -7,14 +7,78 @@ import './Cards.css';
 
 const Cards = () => {
   const [courseGoals, setCourseGoals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
-  const addGoalHandler = (enteredText) => {
+
+  async function addGoalHandler(courseGoals){
+    const response = await fetch('https://tinyurl-82ef0-default-rtdb.firebaseio.com/tinyurl.json', {
+      method: 'POST',
+      body: JSON.stringify(courseGoals),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+    /*
     setCourseGoals((prevGoals) => {
       const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      updatedGoals.unshift({ text: courseGoals, id: Math.random().toString() });
       return updatedGoals;
     });
+    */
   };
+
+  
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        'https://tinyurl-82ef0-default-rtdb.firebaseio.com/meals.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const responseData = await response.json();
+
+      const loadedUrl = [];
+
+      for (const key in responseData) {
+        loadedUrl.push({
+          id: key,
+          children: responseData[key].children,
+        });
+      }
+
+      setCourseGoals(loadedUrl);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section >
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+
 
   const deleteItemHandler = (goalId) => {
     setCourseGoals((prevGoals) => {
@@ -34,12 +98,12 @@ const Cards = () => {
   }
 
   return (
-    <div>
+    <React.Fragment>
       <section id="goal-form">
-        <CourseInput onAddGoal={addGoalHandler} />
+        <CourseInput onAddGoal={addGoalHandler}/>
       </section>
       <section id="goals">{content}</section>
-    </div>
+    </React.Fragment>
   );
 };
 
--- src/components/ShowList/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ src/components/ShowList/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -8,10 +8,24 @@ const CourseGoalItem = (props) => {
   };
 
   return (
-    <li className="goal-item" onClick={deleteHandler}>
-      {props.children}
+    <li className="goal-item" onClick={deleteHandler}>   
+      <ul className="test">
+          <li className="test1">
+            <p>{props.children.length < 50 ? props.children : props.children.substring(0, 50) + "..." }</p>
+          </li>
+
+          <li className="test2">
+            {"https://brief.ly/31K0kwi"}
+          </li>
+
+          <li className="test2"> 
+            <button>copy</button>
+          </li>
+        </ul>
     </li>
   );
 };
 
 export default CourseGoalItem;
+
+
