b9c00cdd3a9c3e80091f0c534cc15f6e8582a2ad
kejin geng <kejingeng@kejins-MacBook-Pro.local>
2021-11-18 21:35:34 -0800

    Update website functions

--- src/components/ShowList/CourseGoals/CourseGoalList/CourseGoalList.js
+++ src/components/ShowList/CourseGoals/CourseGoalList/CourseGoalList.js
@@ -7,17 +7,24 @@ const CourseGoalList = props => {
   return (
     <ul className="goal-list">
       {props.items.map(goal => (
-        <CourseGoalItem key={goal.id} id={goal.id} onDelete={props.onDeleteItem}>
+        <CourseGoalItem 
+        key={goal.id} 
+        id={goal.id} 
+        children={goal.children}
+        onDelete={props.onDeleteItem}
+        >
           <ul className="test">
             <li className="test1">
               {goal.text}
             </li>
+            
             <li className="test2">
-            {"https://brief.ly/"}{goal.text}
+            {"https://brief.ly/31K0kwi"}
             </li>
               <li className="test2"> 
                 <button>copy</button>
               </li>
+           
           </ul>
         </CourseGoalItem>
       ))}
@@ -26,3 +33,5 @@ const CourseGoalList = props => {
 };
 
 export default CourseGoalList;
+
+
--- src/components/ShowList/CourseGoals/CourseInput/CourseInput.js
+++ src/components/ShowList/CourseGoals/CourseInput/CourseInput.js
@@ -1,36 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Button from '../../UI/Button/Button';
 import styles from './CourseInput.module.css';
+import isURL from 'validator/lib/isURL';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
+  const [err, setErr] = useState('');
+  const [showElement, setShowElement] = useState(true);
+  const childrenRef = useRef('');
+
+
+  useEffect(() => {
+    setTimeout(function () {
+      setShowElement(false);
+    }, 5000);
+  }, []);
+
+  const limit = (string='', limit=0) => {
+    return string.substring(0, limit) + "...";
+  }
 
   const goalInputChangeHandler = (event) => {
-    if (event.target.value.trim().length > 0) {
+    if (event.target.value.trim().length > 0 ) {
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
   };
+  
+  
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    if (enteredValue.trim().length === 0) {
+    if (enteredValue.trim().length === 0 || !isURL(enteredValue)) {
       setIsValid(false);
+      setErr('Invalid URL');
       return;
+    }else {
+      setIsValid(true);
+      if(enteredValue.trim().length < 50 && isURL(enteredValue)){
+        props.onAddGoal(enteredValue);
+      }else if(enteredValue.trim().length > 50 && isURL(enteredValue)){
+        props.onAddGoal(limit(enteredValue, 50));
+      }
     }
-    props.onAddGoal(enteredValue);
   };
+
+  
+  
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
-  
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input 
+        type="text" 
+        onChange={goalInputChangeHandler}
+        ref={childrenRef}
+        />
+        
       </div>
       <Button type="submit">Shorten</Button>
-      
+       <div>
+        {showElement ? (
+          <div>
+            {err}
+          </div>
+        ) : (
+          <div></div>
+        )}{" "}
+        </div>
     </form>
   );
 };
